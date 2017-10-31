2702d9d8a5036b9e48dafd033615281e34bc4b53
ThomasAnkcorn <thomasankcorn@gmail.com>
2017-10-31 20:25:16 +0000

    formatted code better

--- requests/getphoto.js
+++ requests/getphoto.js
@@ -1,23 +1,25 @@
-const fs = require('fs-extra')
-const path = require('path')
-const sharp = require('sharp')
+const fs = require("fs-extra")
+const path = require("path")
+const sharp = require("sharp")
 
-module.exports = async function (req, res) {
-    const photoNumber = req.params.id
-    const width_string = req.params.width || 1000
-    const height_string = req.params.height || 600
-    const width = parseInt(width_string)
-    const height = parseInt(height_string)
+module.exports = async function(req, res) {
+  const photoNumber = req.params.id
+  const width_string = req.params.width || 1000
+  const height_string = req.params.height || 600
+  const width = parseInt(width_string)
+  const height = parseInt(height_string)
 
-    try {
-        const photosList = await fs.readdir(__dirname + '/../photos/')
-        console.log('Fetching Photo ' + path.resolve('./photos/' + photosList[photoNumber]))
-        const resizedPhotoBuffer = await sharp(path.resolve('./photos/' + photosList[photoNumber]))
-            .resize(width, height)
-            .toBuffer()
-        res.send(resizedPhotoBuffer)
-    } catch (e) {
-        console.error(e)
-        res.status(500).send(e)
-    }
+  try {
+    const photosList = await fs.readdir(__dirname + "/../photos/")
+    const photoPath = path.resolve("./photos/" + photosList[photoNumber])
+    console.log("Fetching Photo " + photoPath)
+    const resizedPhotoBuffer = await sharp(photoPath)
+      .resize(width, height)
+      .toBuffer()
+
+    res.send(resizedPhotoBuffer)
+  } catch (e) {
+    console.error(e)
+    res.status(500).send(e)
+  }
 }
